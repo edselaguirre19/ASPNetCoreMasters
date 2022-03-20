8bdd8695af3fd07f484fed8b34fd2d33a5d208cb
edselaguirre19 <91943090+edselaguirre19@users.noreply.github.com>
2022-03-20 21:36:48 +0800

    add create logger to program.cs serilog config

--- ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList.API/Program.cs
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList.API/Program.cs
@@ -15,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console()
     .WriteTo.Seq("http://localhost:5341/")
+    .CreateLogger()
 );
 
 // Add services to the container.
